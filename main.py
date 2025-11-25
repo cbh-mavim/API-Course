5000e39e2f310349e6f3ae5ed21573284443e7ea
Chinmay S B <cbh.mavim@gmail.com>
2025-11-25 20:48:13 +0530

    Added Routes for each so the debugging is easy and main.py is clean

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from fastapi import FastAPI,status,Response
-from enum import Enum
-from typing import Optional
-
+from fastapi import FastAPI
+from routers import blog_get,blog_post
 
 app = FastAPI()
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
-class BlogType(str, Enum):
-    short = "short"
-    story = "story"
-    howto = "howto"
 
 @app.get("/")
 def root_folder():
@@ -16,30 +12,3 @@ def root_folder():
         "message" : "Hey!"
     }
 
-@app.get("/blogs/all",tags=['blog'],summary='Reterive all blogs',description='This api call simulates',response_description='The list of available blogs')
-def get_all_blogs(page:int = 2,page_size:Optional[int] = None):
-    return {
-        "message" : f"All {page} are {page_size}"
-    }
-
-
-@app.get("/blog/type/{btype}",tags=['blog'])
-def get_blog_type(type: BlogType):
-    return {
-        "message" : f"Blog Type : {type}"
-    }
-
-@app.get("/blog/{id}",status_code=status.HTTP_200_OK,tags=['blog'])
-def get_blog(id:int,response:Response):
-    if id> 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'error' : f'Blog {id} not found!!'}
-    else:
-        response.status_code = status.HTTP_200_OK
-        return {'message': 'Default!!'}
-
-@app.get("/blog/{id}/comments/{comment_id}",tags=['blog','comment'])
-def get_blog_comment(id:int,comment_id:int,valid:bool= True, username: Optional[str]= None):
-    return {
-        'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'
-    }
---
+++ routers/blog_get.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter,status,Response
+from typing import Optional
+from enum import Enum
+
+class BlogType(str, Enum):
+    short = "short"
+    story = "story"
+    howto = "howto"
+
+router = APIRouter(
+    prefix= '/blog',
+    tags=['blog']
+)
+
+@router.get("/all",summary='Reterive all blogs',description='This api call simulates',response_description='The list of available blogs')
+def get_all_blogs(page:int = 2,page_size:Optional[int] = None):
+    return {
+        "message" : f"All {page} are {page_size}"
+    }
+
+@router.get("/type/{btype}")
+def get_blog_type(type: BlogType):
+    return {
+        "message" : f"Blog Type : {type}"
+    }
+
+@router.get("/{id}",status_code=status.HTTP_200_OK)
+def get_blog(id:int,response:Response):
+    if id> 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error' : f'Blog {id} not found!!'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': 'Default!!'}
+
+@router.get("/{id}/comments/{comment_id}",tags=['comment'])
+def get_blog_comment(id:int,comment_id:int,valid:bool= True, username: Optional[str]= None):
+    return {
+        'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'
+    }
