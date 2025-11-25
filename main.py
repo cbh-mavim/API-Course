b474e7902271af69a57139e43ed2811c055b50df
Chinmay S B <cbh.mavim@gmail.com>
2025-11-25 20:08:32 +0530

    Added status code, tags, summary, description & response description to the file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI,status,Response
 from enum import Enum
 from typing import Optional
+
 
 app = FastAPI()
 
@@ -15,26 +16,29 @@ def root_folder():
         "message" : "Hey!"
     }
 
-@app.get("/blogs/all")
+@app.get("/blogs/all",tags=['blog'],summary='Reterive all blogs',description='This api call simulates',response_description='The list of available blogs')
 def get_all_blogs(page:int = 2,page_size:Optional[int] = None):
     return {
         "message" : f"All {page} are {page_size}"
     }
 
 
-@app.get("/blog/type/{btype}")
+@app.get("/blog/type/{btype}",tags=['blog'])
 def get_blog_type(type: BlogType):
     return {
         "message" : f"Blog Type : {type}"
     }
 
-@app.get("/blog/{id}")
-def get_blog(id:int):
-    return {
-        "message" : f"Blog with id: {id}"
-    }
+@app.get("/blog/{id}",status_code=status.HTTP_200_OK,tags=['blog'])
+def get_blog(id:int,response:Response):
+    if id> 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error' : f'Blog {id} not found!!'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': 'Default!!'}
 
-@app.get("/blog/{id}/comments/{comment_id}")
+@app.get("/blog/{id}/comments/{comment_id}",tags=['blog','comment'])
 def get_blog_comment(id:int,comment_id:int,valid:bool= True, username: Optional[str]= None):
     return {
         'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'
