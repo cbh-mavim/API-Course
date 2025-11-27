ac5c0671df49be8f5cae787de4f5b876fe3bf39e
Chinmay S B <cbh.mavim@gmail.com>
2025-11-27 22:39:53 +0530

    Database details added

---
+++ app/routers/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from app.schemas.schemas import UserDisplay,UserBase
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.db import db_user
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"]
+)
+
+@router.post("/",response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
