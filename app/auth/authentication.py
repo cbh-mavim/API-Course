c7cd21cd6d35ec92053e1abdf5346c26654bdf06
Chinmay S B <cbh.mavim@gmail.com>
2025-12-03 20:12:20 +0530

    Added Authentication to the API Endpoints

---
+++ app/auth/authentication.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from app.auth import oauth2
+from sqlalchemy.orm.session import Session
+from app.db.database import get_db
+from app.models.models import DbUser
+from app.db.hash import Hash
+
+router = APIRouter(
+   tags=["authentication"] 
+)
+
+@router.post("/token")
+def get_token(request: OAuth2PasswordRequestForm = Depends(),db:Session = Depends(get_db)):
+    user = db.query(DbUser).filter(DbUser.username == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The user with username : {request.username} was not found!!")
+    if not Hash.verify(user.password,request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Incorrect Password")
+    
+    access_token = oauth2.create_access_token(data={
+        'sub' :user.username
+    })
+
+    return {
+        'access_token' : access_token,
+        'token_type' : 'bearer',
+        'user_id': user.id,
+        'user_name' : user.username
+    }
